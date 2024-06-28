a3606a4bc774b747ff64863981585c70d4e37e98
adellinekania <adellinekaniasetiyawan@gmail.com>
2024-06-28 13:14:57 +0700

    exclude non main jar repository paths

--- src/main/java/org/codehaus/mojo/license/artifactory/ArtifactoryDsl.java
+++ src/main/java/org/codehaus/mojo/license/artifactory/ArtifactoryDsl.java
@@ -6,12 +6,18 @@ import org.jfrog.artifactory.client.Artifactory;
 import org.jfrog.artifactory.client.ArtifactoryClientBuilder;
 import org.jfrog.artifactory.client.model.RepoPath;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArtifactoryDsl {
     private final Log log;
     private final Artifactory artifactory;
+
+    private static final List<String> EXCLUDED_CLASSIFIERS = Arrays.asList(
+            "-sources", "-javadoc", "-tests", "-shaded", "-bundle", "-native"
+    );
 
     public ArtifactoryDsl(Log log, String artifactRepositoryUrl, String artifactRepositoryAccessToken){
         this.log = log;
@@ -34,16 +40,25 @@ public class ArtifactoryDsl {
                     .classifier("*.jar")
                     .doSearch();
 
-            if (!repoPaths.isEmpty()) {
+            // Filter to get the main JAR file repo path
+            List<RepoPath> mainRepoPaths = repoPaths.stream()
+                    .filter(repoPath -> {
+                        String itemPath = repoPath.getItemPath();
+                        String fileName = itemPath.substring(itemPath.lastIndexOf('/') + 1);
+                        return EXCLUDED_CLASSIFIERS.stream().noneMatch(fileName::contains);
+                    })
+                    .collect(Collectors.toList());
+
+            if (!mainRepoPaths.isEmpty()) {
                 log.debug("Found repo paths:");
-                for (RepoPath repoPath : repoPaths) {
+                for (RepoPath repoPath : mainRepoPaths) {
                     log.debug("\t" + "default/" + repoPath.getRepoKey() + "/" + repoPath.getItemPath());
                 }
             } else {
                 log.debug("Can't find any artifact for project " + project.toString());
             }
 
-            return repoPaths;
+            return mainRepoPaths;
         } catch (Exception e) {
             log.error("Error occurred while searching for artifacts ", e);
             return Collections.emptyList();
