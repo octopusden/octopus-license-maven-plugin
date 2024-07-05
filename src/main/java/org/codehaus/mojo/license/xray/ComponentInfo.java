8c90568402b49e44639e91c95368a83eef079286
adellinekania <adellinekaniasetiyawan@gmail.com>
2024-07-05 11:15:52 +0700

    handle null response data

--- src/main/java/org/codehaus/mojo/license/xray/ComponentInfo.java
+++ src/main/java/org/codehaus/mojo/license/xray/ComponentInfo.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Collections;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,7 +13,7 @@ public class ComponentInfo {
     private List<ComponentData> data;
 
     public List<ComponentData> getData() {
-        return data;
+        return data == null ? Collections.emptyList() : data;
     }
 
     public void setData(List<ComponentData> data) {
--- src/test/java/org/codehaus/mojo/license/xray/XrayLicenseProcessorTest.java
+++ src/test/java/org/codehaus/mojo/license/xray/XrayLicenseProcessorTest.java
@@ -52,6 +52,15 @@ public class XrayLicenseProcessorTest {
     }
 
     @Test
+    public void testParseNullDataJSON() throws IOException {
+        String data = loadToString("xrayLicenseEmptyInfo.json");
+        ComponentInfo componentInfo = licenseProcessor.parseJSON(data);
+
+        Assert.assertNotNull(componentInfo);
+        Assert.assertEquals(0, componentInfo.getData().size());
+    }
+
+    @Test
     public void testGetLicenseFromJson() throws IOException {
         String data = loadToString("xrayLicenseInfo.json");
 
@@ -76,6 +85,21 @@ public class XrayLicenseProcessorTest {
         Assert.assertNotNull(licenses);
         Assert.assertEquals(1, licenses.size());
         Assert.assertEquals("Apache-2.0", licenses.get(0).getName());
+    }
+
+    @Test
+    public void testGetNullLicenseByProject() {
+        if (artifactUrl == null || accessToken == null) return;
+
+        MavenProject mavenProject = new MavenProject();
+        mavenProject.setGroupId("com.oracle.jdbc");
+        mavenProject.setArtifactId("ojdbc8");
+        mavenProject.setVersion("18.3.0.0");
+
+        List<License> licenses = licenseProcessor.getLicensesByProject(mavenProject);
+
+        Assert.assertNotNull(licenses);
+        Assert.assertEquals(0, licenses.size());
     }
 
 
